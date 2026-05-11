eb453c9816fbd26b083a6719efd5b621d0e04a93
fumeboy <fumeboy@qq.com>
2026-05-11 13:37:13 +0800

    rm unused file

--- a.sh
+++
@@ -1,9 +0,0 @@
-curl 'https://search.bytedance.net/gpt/openapi/online/v2/crawl?ak=5Tbfs4d7ysO2fI7saWuyT4kRa3rv0NXe_GPT_AK' \
--H 'Content-Type: application/json' \
--H "Authorization: Bearer ${V_API_KEY}" \
--d '{                                                                                    
-"model": "gpt-5.5-2026-04-24",                                                         
-"messages": [                                                                          
-    { "role": "user", "content": "ping" }                                                
-]                                                                                      
-}'  
